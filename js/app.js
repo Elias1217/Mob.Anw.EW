6212c266a9b5c576220ec155c8831b72ee486879
Elias1217 <93335029+Elias1217@users.noreply.github.com>
2021-11-04 18:52:45 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -91,7 +91,7 @@ function takePhoto() {
     .then(blob => {
       var theImageTag = document.getElementById("imageTag");
       theImageTag.src = URL.createObjectURL(blob);;
-      localStorage.setItem("theImagetag.src", "Bildsave");
+      localStorage.setItem("theImagetag", "Bildsave");
     })
     .catch(err => alert('Error: ' + err));
 }
@@ -99,7 +99,7 @@ function takePhoto() {
 
   
 window.onload = function() {
- var picture = localStorage.getItem('theImagetag.src');
+ var picture = localStorage.getItem('theImagetag');
  var image = document.createElement('img');
  image.src = picture;
  document.body.appendChild(image);
