3bedbb7a761f44ec06da39391091b9938d68a46b
Elias1217 <93335029+Elias1217@users.noreply.github.com>
2021-11-11 17:51:04 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -93,7 +93,7 @@ function takePhoto() {
     .then(blob => {
       var theImageTag = document.getElementById("imageTag");
       theImageTag.src = URL.createObjectURL(blob, {autorevoke : false});
-      var myFile = blobToFile(theImageTag.src, "my-image.png");
+      var myFile = blobToFile(theImageTag, "my-image.png");
       localStorage.setItem("ImageTag", myFile);
     })
     .catch(err => alert('Error: ' + err));
