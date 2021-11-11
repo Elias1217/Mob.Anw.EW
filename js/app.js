3cab58ce075d52d2aba3154936d38004ccbf7a0a
Elias1217 <93335029+Elias1217@users.noreply.github.com>
2021-11-11 17:06:50 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -152,3 +152,35 @@ function deviceOrientationHandler (eventData) {
   logo.style.MozTransform = "rotate(" + tiltLR + "deg)";
   logo.style.transform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
 }
+
+func startGyros() {
+   if motion.isGyroAvailable {
+      self.motion.gyroUpdateInterval = 1.0 / 60.0
+      self.motion.startGyroUpdates()
+
+      // Configure a timer to fetch the accelerometer data.
+      self.timer = Timer(fire: Date(), interval: (1.0/60.0), 
+             repeats: true, block: { (timer) in
+         // Get the gyro data.
+         if let data = self.motion.gyroData {
+            let x = data.rotationRate.x
+            let y = data.rotationRate.y
+            let z = data.rotationRate.z
+
+            // Use the gyroscope data in your app. 
+         }
+      })
+
+      // Add the timer to the current run loop.
+      RunLoop.current.add(self.timer!, forMode: .defaultRunLoopMode)
+   }
+}
+
+func stopGyros() {
+   if self.timer != nil {
+      self.timer?.invalidate()
+      self.timer = nil
+
+      self.motion.stopGyroUpdates()
+   }
+}
