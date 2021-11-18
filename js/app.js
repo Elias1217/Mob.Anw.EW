3f5564935edbaec64cf53613a20165ceaf1b249a
Elias1217 <93335029+Elias1217@users.noreply.github.com>
2021-11-18 17:29:46 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -201,6 +201,7 @@ function requestPersistence() {
   var fileReader = new FileReader();
   fileReader.onload = function(fileLoadedEvent){
       var textFromFileLoaded = fileLoadedEvent.target.result;
+    document.getElementById("inputTextToSave").value = textFromFileLoaded;
   };
 
   fileReader.readAsText(fileToLoad, "UTF-8");
