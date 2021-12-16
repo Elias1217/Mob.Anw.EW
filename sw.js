5097633441b98a89c37b13c30918c3cef17290fd
Elias1217 <93335029+Elias1217@users.noreply.github.com>
2021-12-16 17:12:02 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -15,6 +15,22 @@ const assets = [
   "/images/coffee9.jpg"
 ];
 
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+
+
+
 self.addEventListener("install", installEvent => {
   console.log(installEvent);
   installEvent.waitUntil(
