6e1fa86d464abfa4463610ddd5f2f3d76aa4d1ee
Elias1217 <93335029+Elias1217@users.noreply.github.com>
2021-12-16 16:59:05 +0100

    Create sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,31 @@
 
-self.addEventListener('install', event => {
-  console.log('V1 installing…');
+const Bankaccount = "bankaccount";
+const assets = [
+  "/",
+  "/index.html",
+  "/css/styles.css"
+];
 
-  // cache a cat SVG
-  event.waitUntil(
-    caches.open('static-v1').then(cache => cache.add('/cat.svg'))
+self.addEventListener("install", installEvent => {
+  console.log(installEvent);
+  installEvent.waitUntil(
+    caches
+      .open(BankAccount)
+      .then(cache => {
+        cache.addAll(assets);
+      })
+      .catch(console.log)
   );
 });
 
-self.addEventListener('activate', event => {
-  console.log('V1 now ready to handle fetches!');
-});
-
-self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-
-  // serve the cat SVG from the cache if the request is
-  // same-origin and the path is '/dog.svg'
-  if (url.origin == location.origin && url.pathname == '/dog.svg') {
-    event.respondWith(caches.match('/cat.svg'));
-  }
+self.addEventListener("fetch", event => {
+  // console.log(event.request);
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(res => {
+        return res || fetch(event.request);
+      })
+      .catch(console.log)
+  );
 });
