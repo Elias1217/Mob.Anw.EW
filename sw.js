59c359bab1d8b5d19eaf81e5b2f177089ec5b257
Elias1217 <93335029+Elias1217@users.noreply.github.com>
2021-12-16 17:04:51 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,25 @@
-
-const Bankaccount = "bankaccount";
+const staticDevCoffee = "dev-coffee-site-v1";
 const assets = [
   "/",
   "/index.html",
-  "/css/styles.css"
+  "/css/styles.css",
+  "/js/app.js",
+  "/images/coffee1.jpg",
+  "/images/coffee2.jpg",
+  "/images/coffee3.jpg",
+  "/images/coffee4.jpg",
+  "/images/coffee5.jpg",
+  "/images/coffee6.jpg",
+  "/images/coffee7.jpg",
+  "/images/coffee8.jpg",
+  "/images/coffee9.jpg"
 ];
 
 self.addEventListener("install", installEvent => {
   console.log(installEvent);
   installEvent.waitUntil(
     caches
-      .open(BankAccount)
+      .open(staticDevCoffee)
       .then(cache => {
         cache.addAll(assets);
       })
